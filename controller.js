24d9e12d7dac6ada473e2bdd1ad9da4a4f0460c5
jamesponwith <jponwith@sandiego.edu>
2018-12-27 10:57:05 -0700

    changed method for js

--- controller.js
+++ controller.js
@@ -9,9 +9,9 @@ window.onload = () => {
   hamburger.addEventListener('click', () => toggle(navMenu, 'is-active'));
   Array.from(navItems).forEach(e => e.addEventListener('click', toggleNav));
 
-  var instance = M.Parallax.getInstance(elem);
-  $(document).ready(function(){
-    $('.parallax').parallax();
-  });
+  document.addEventListener('DOMContentLoaded', function() {
+      var elems = document.querySelectorAll('.parallax');
+      var instances = M.Parallax.init(elems, options);
+    });
 
 }
