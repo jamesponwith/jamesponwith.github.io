9b47faf2bb64a297f23c21db9dcc63b978814241
jamesponwith <jponwith@sandiego.edu>
2018-12-28 15:31:31 -0700

    maybe blue

--- assets/js/app.js
+++ assets/js/app.js
@@ -50,7 +50,7 @@ particlesJS('particles-js',
       "line_linked": {
         "enable": true,
         "distance": 150,
-        "color": "#888",
+        "color": "#033",
         "opacity": 0.4,
         "width": 1
       },
