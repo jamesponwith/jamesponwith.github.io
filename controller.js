98b59a4ec5d19969d97d5ec334b7132e7d1bedf4
jamesponwith <jponwith@sandiego.edu>
2018-12-28 01:52:19 -0700

    good stuff hopefully

--- controller.js
+++ controller.js
@@ -9,6 +9,84 @@ window.onload = () => {
   hamburger.addEventListener('click', () => toggle(navMenu, 'is-active'));
   Array.from(navItems).forEach(e => e.addEventListener('click', toggleNav));
 
+  $(function (){
+
+    "use strict";
+
+    var wind = $(window);
+
+    //smooth scroll
+    $('.navbar-nav').singlePageNav({
+        speed:1000,
+        currentClass:'active',
+        offset:60
+    });
+
+
+    // navbar scrolling background
+    wind.on("scroll",function () {
+
+        var bodyScroll = wind.scrollTop(),
+            navbar = $(".navbar");
+
+        if(bodyScroll > 300){
+
+            navbar.show();
+
+        }else{
+
+            navbar.hide();
+        }
+    });
+
+    //smooth button scroll
+    $('.button-scroll').on('click', function(){
+
+        var scrollTo = $(this).attr('data-scrollTo');
+
+        $('body, html').animate({
+
+        "scrollTop": $('#'+scrollTo).offset().top - 60
+        }, 1000 );
+
+    });
+
+
+    // progress bar
+    wind.on('scroll', function () {
+        $(".progress-main .progress-bar").each(function () {
+            var bottom_of_object =
+            $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window =
+            $(window).scrollTop() + $(window).height();
+            var myVal = $(this).attr('data-value');
+            if(bottom_of_window > bottom_of_object) {
+                $(this).css({
+                  width : myVal
+                });
+            }
+        });
+    });
+
+
+    // magnificPopup
+    $('.v-middle').magnificPopup({
+      delegate: 'a',
+      type: 'image'
+    });
+
+
+    // counterUp
+    $('.counter').counterUp({
+        delay: 10,
+        time: 1500
+    });
+
+    // stellar
+    wind.stellar();
+
+});
+
   // document.addEventListener('DOMContentLoaded', function() {
   //     var elems = document.querySelectorAll('.parallax');
   //     var instances = M.Parallax.init(elems, options);
