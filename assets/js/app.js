9e2c0b445d7df83358056e29e2e0efaf9b81b4d5
jamesponwith <jamesponwith@gmail.com>
2021-02-10 19:53:27 -0500

    made fix to particles

--- assets/js/app.js
+++ assets/js/app.js
@@ -73,7 +73,7 @@ particlesJS('particles-js',
       "events": {
         "onhover": {
           "enable": true,
-          "mode": "grab"
+          "mode": "repulse"
         },
         "onclick": {
           "enable": true,
